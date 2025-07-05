66e321c7290877e6d96eb1f7e81a9c24695c5075
AnPQ <anpham1122000@gmail.com>
2025-07-05 23:12:36 +0700

    group api

--- main.go
+++ main.go
@@ -1,49 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	v1handler "pqan.com/go-api/api/v1/handler"
 )
-
-func GetUsers(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"messsage": "List all users",
-	})
-}
-
-func GetUser(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"messsage": "List user detail",
-	})
-}
-
-func CreateUser(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"messsage": "Create user",
-	})
-}
-
-func UpdateUser(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"messsage": "Update user",
-	})
-}
-
-func DeleteUser(c *gin.Context) {
-	c.JSON(http.StatusNoContent, gin.H{
-		"messsage": "Update user",
-	})
-}
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/api/v1/users", GetUsers)
-	router.GET("/api/v1/users/:id", GetUser)
-	router.POST("/api/v1/users", CreateUser)
-	router.PUT("/api/v1/users/:id", UpdateUser)
-	router.DELETE("/api/v1/users/:id", DeleteUser)
+	v1 := router.Group("/api/v1")
+	{
+		userHandlerV1 := v1handler.NewUserHandler()
+		v1.GET("/users", userHandlerV1.GetUsers)
+		v1.GET("/users/:id", userHandlerV1.GetUser)
+		v1.POST("/users", userHandlerV1.CreateUser)
+		v1.PUT("/users/:id", userHandlerV1.UpdateUser)
+		v1.DELETE("/users/:id", userHandlerV1.DeleteUser)
+	}
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
