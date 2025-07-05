4502820482352401d31703838dc07d2533fae66e
AnPQ <anpham1122000@gmail.com>
2025-07-05 20:18:10 +0700

    url, path, params

--- main.go
+++ main.go
@@ -9,5 +9,14 @@ func main() {
 			"message": "pong",
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+
+	router.GET("/users/:id", func(c *gin.Context) {
+		userId := c.Param("id")
+		age := c.Query("age")
+		c.JSON(200, gin.H{
+			"userId": userId,
+			"age":    age,
+		})
+	})
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
