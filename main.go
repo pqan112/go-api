ec6a59c1869396b68b27bb12639aa7bfcde24b3e
AnPQ <anpham1122000@gmail.com>
2025-07-05 22:57:56 +0700

    user-router

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"messsage": "List all users",
+	})
+}
+
+func GetUser(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"messsage": "List user detail",
+	})
+}
+
+func CreateUser(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{
+		"messsage": "Create user",
+	})
+}
+
+func UpdateUser(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"messsage": "Update user",
+	})
+}
+
+func DeleteUser(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{
+		"messsage": "Update user",
+	})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
-	router.GET("/users/:id", func(c *gin.Context) {
-		userId := c.Param("id")
-		age := c.Query("age")
-		c.JSON(200, gin.H{
-			"userId": userId,
-			"age":    age,
-		})
-	})
+	router.GET("/api/v1/users", GetUsers)
+	router.GET("/api/v1/users/:id", GetUser)
+	router.POST("/api/v1/users", CreateUser)
+	router.PUT("/api/v1/users/:id", UpdateUser)
+	router.DELETE("/api/v1/users/:id", DeleteUser)
+
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
