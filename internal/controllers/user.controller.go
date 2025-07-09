ed8c9870d1951f937775f053371c97d016dd7c38
An Pham <pqan112@gmail.com>
2025-07-09 14:52:30 +0700

    a

--- internal/controllers/user.controller.go
+++ internal/controllers/user.controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"pqan.com/go-api/internal/services"
 )
@@ -17,5 +19,6 @@ func NewUserController(service *services.UserService) *UserController {
 
 func (uc *UserController) GetUsers(ctx *gin.Context) {
 	// ctx.JSON(http.StatusOK, gin.H{"message": uc.service.GetUsers})
+	log.Print("aa")
 	uc.service.GetUsers()
 }
